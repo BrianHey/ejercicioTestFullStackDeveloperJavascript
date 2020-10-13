bde108845e3bbfd0c69e0fa898ac45072df842bf
Brian Habib <brianhft20@gmail.com>
2020-10-13 14:32:57 -0300

    inicio de test de habilidades

---
+++ index.js
@@ -0,0 +1,23 @@
+const registros = [
+  {
+    id: 1,
+    nombre: "Martillo",
+    date: new Date("2015-05-02"),
+  },
+
+  {
+    id: 2,
+    nombre: "Patineta",
+    date: new Date("2011-05-02"),
+  },
+  {
+    id: 3,
+    nombre: "Monitor",
+    date: new Date("2008-05-02"),
+  },
+  {
+    id: 4,
+    nombre: "Carro volador con propursor a chorro",
+    date: new Date("2035-05-02"),
+  },
+];
