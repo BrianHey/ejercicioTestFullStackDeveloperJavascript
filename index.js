a8f2b16ca2669710501490cfdfbf0476d1e9a35e
PedroManuel <pedromanuelrivas@gmail.com>
2020-10-13 16:11:22 -0300

    Codigo BAMBAM

--- index.js
+++ index.js
@@ -21,3 +21,24 @@ const registros = [
     date: new Date("2035-05-02"),
   },
 ];
+
+var regOrdenados = registros.sort(function (a, b) { return a.date - b.date });
+console.log(regOrdenados)
+
+var cuerpoTabla = document.getElementById("tablaOrdenada")
+
+for (let i = 0; i <= registros.length; i++) {
+  var fila = document.createElement("tr")
+  var columna1 = document.createElement("td")
+  var columna2 = document.createElement("td")
+  var columna3 = document.createElement("td")
+  columna1.innerHTML = regOrdenados[i].id
+  columna2.innerHTML = regOrdenados[i].nombre
+  columna3.innerHTML = regOrdenados[i].date
+
+  fila.appendChild(columna1)
+  fila.appendChild(columna2)
+  fila.appendChild(columna3)
+
+  cuerpoTabla.appendChild(fila)
+}
